e9189724d3b4298da3d2ae5670d588d8267dbe17
neha <nehambk@gmail.com>
2020-11-09 18:41:36 +0530

    first commit

---
+++ src/test/java/umbrellaproject/Mavenjava/RestAPITest.java
@@ -0,0 +1,19 @@
+package umbrellaproject.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+	
+	@Test
+	public void postJira()
+	{
+		System.out.println("postJira");
+	}
+	
+	@Test
+	public void deleteTwitter()
+	{
+		System.out.println("deleteTwitter");
+	}
+
+}
