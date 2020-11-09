12f5cdffc4bfd7f5a715c35fc416cf16f3105792
neha <nehambk@gmail.com>
2020-11-09 22:36:35 +0530

    updated

--- src/test/java/umbrellaproject/Mavenjava/SeleniumTest.java
+++ src/test/java/umbrellaproject/Mavenjava/SeleniumTest.java
@@ -8,6 +8,8 @@ public class SeleniumTest {
 	public void BrowserAutomation()
 	{
 		System.out.println("BrowserAutomation");
+		System.out.println("BrowserAutomation1");
+		System.out.println("BrowserAutomation2");
 	}
 	
 	@Test
